cf556e2ced532d163b1be0f04c0c106e7734ada9
Rohan Ghadge <113715810+Rohan1366@users.noreply.github.com>
2023-05-26 17:33:44 +0530

    added

---
+++ routes/authRoute.js
@@ -0,0 +1,25 @@
+  import  express  from "express";
+  import { registerController } from "../controllers/authController.js";
+  import { loginController } from "../controllers/authController.js";
+  import { requireSignIn } from "../middlewares/authMiddleware.js";
+  import { isAdmin } from "../middlewares/authMiddleware.js";
+  //router object
+const router = express.Router();
+
+//routing
+//REGISTER || METHOD POST
+router.post("/register", registerController);
+
+//LOGIN || POST
+router.post("/login", loginController);
+
+//protected User route auth
+router.get("/user-auth", requireSignIn, (req, res) => {
+    res.status(200).send({ ok: true });
+  });
+  //protected Admin route auth
+  router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
+    res.status(200).send({ ok: true });
+  });
+
+  export default router;
---
+++ server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import colors from 'colors'
+import dotenv from "dotenv";
+import morgan  from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js"
+//rest object
+const app = express();
+
+//configure env
+dotenv.config();
+
+// database config
+connectDB()
+
+// middleware
+app.use(express.json())
+app.use(morgan('dev'))
+
+//routes
+app.use("/api/v1/auth", authRoutes);
+
+app.get('/',(req,res)=>{
+    res.send({
+        messege:"welcom"
+    })
+})
+
+//Port
+const PORT = process.env.PORT || 8080;
+
+//run listen
+app.listen(PORT,()=>{
+    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white)
+})
